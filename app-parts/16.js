d1d58bb413899b15600d5a2481d36e7f06ad2cf2
chylerbot-beep <chylerbot@gmail.com>
2026-07-13 16:29:06 +0800

    Make Show all walls fully reset camera cutaway

--- app-parts/16.js
+++ app-parts/16.js
@@ -5,3 +5,13 @@
         scene.updateMatrixWorld(true);camera.updateMatrixWorld(true);
         return automaticBlockingWallIdsBeforeMatrixRefresh(settings);
       };
+
+      // "Show all walls" is an absolute reset: clear manual overrides and turn off
+      // automatic cutaway so the complete architectural shell is visible again.
+      const syncCameraCutawayControlsBeforeShowAll=syncCameraCutawayControls;
+      syncCameraCutawayControls=function(){
+        syncCameraCutawayControlsBeforeShowAll();
+        const settings=ensureCameraCutawaySettings();
+        if($('showAllCameraWalls'))$('showAllCameraWalls').disabled=!settings.enabled&&!settings.hiddenWallIds.length;
+      };
+      if($('showAllCameraWalls'))$('showAllCameraWalls').onclick=()=>updateCutawaySetting('show all camera walls',settings=>{settings.enabled=false;settings.hiddenWallIds=[];});
